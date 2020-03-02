975a65f8d26f1bd6ff6a0c5b898550f297cd3eb1
hidnarola <hda@narola.email>
2020-03-02 18:15:33 +0530

    gatsby-image

--- plugins/gatsby-builton/gatsby-node.js
+++ plugins/gatsby-builton/gatsby-node.js
@@ -2,7 +2,7 @@ const Builton = require('@builton/core-sdk')
 const { createRemoteFileNode } = require('gatsby-source-filesystem')
 
 exports.sourceNodes = async (
-  { actions, createNodeId, createContentDigest },
+  { actions, createNodeId, createContentDigest, store, cache },
   { apiKey }
 ) => {
   const { createNode } = actions
@@ -11,8 +11,8 @@ exports.sourceNodes = async (
     apiKey
   })
 
-  const processProduct = product => {
-    // console.log('product => ', product)
+  const processProduct = async product => {
+    console.log('product => ', product)
 
     const nodeId = createNodeId(product.id)
     const nodeContent = JSON.stringify(product)
@@ -30,40 +30,41 @@ exports.sourceNodes = async (
 
     return nodeData
   }
+
   const productList = await builton.products.getAll()
   const products = productList.current
-  return products.map(product => {
-    const nodeData = processProduct(product)
+  return products.map(async product => {
+    const nodeData = await processProduct(product)
     createNode(nodeData)
   })
 }
-// exports.onCreateNode = async ({ node, actions, store, cache }) => {
-//   // if the node is not DogImage, we don't wanna do anything
-//   if (node.internal.type !== 'BuiltonProduct') {
-//     return
-//   }
+exports.onCreateNode = async ({ node, actions, store, cache }) => {
+  if (node.internal.type !== 'BuiltonProduct') {
+    return
+  }
 
-//   console.log('node => ', node.internal.type === 'BuiltonProduct' && node)
+  console.log('node => ', node.internal.type === 'BuiltonProduct' && node)
 
-//   const { createNode } = actions
-//   // download image and create a File node
-//   // with gatsby-transformer-sharp and gatsby-plugin-sharp
-//   // that node will become an ImageSharp
-//   let fileNode
-//   if (node.internal.type === 'BuiltonProduct') {
-//     node.media.map(async med => {
-//       return (fileNode = await createRemoteFileNode({
-//         url: med.url,
-//         store,
-//         cache,
-//         createNode,
-//         createNodeId: id => `testId-${id}`
-//       }))
-//     })
-//     if (fileNode) {
-//       // link File node to DogImage node
-//       // at field image
-//       node.image___NODE = fileNode.id
-//     }
-//   }
-// }
+  const { createNode } = actions
+
+  if (node.internal.type === 'BuiltonProduct' && node.media.length > 0) {
+    await Promise.all(
+      node.media &&
+        node.media.map(async med => {
+          let fileNode
+          fileNode = await createRemoteFileNode({
+            url: med.url,
+            store,
+            cache,
+            createNode,
+            createNodeId: id => `testId-${id}`
+          })
+          if (fileNode) {
+            console.log('In fileNode If condition')
+            node.image___NODE = fileNode.id
+            return med
+          }
+        })
+    )
+  }
+}
